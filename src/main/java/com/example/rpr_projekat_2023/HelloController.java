fc5e582d3d310659f96ac6c85eb98755d13dfc17
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-07 13:52:37 +0200

    Initial commit

---
+++ src/main/java/com/example/rpr_projekat_2023/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.rpr_projekat_2023;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
