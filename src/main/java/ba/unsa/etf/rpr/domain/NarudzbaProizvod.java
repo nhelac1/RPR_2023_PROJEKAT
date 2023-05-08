afd6d188e58f149588165f5b3edfbdbbb38cd117
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-08 21:57:59 +0200

    Zavrsena klasa NarudzbaProizvod - domain

--- src/main/java/ba/unsa/etf/rpr/domain/NarudzbaProizvod.java
+++ src/main/java/ba/unsa/etf/rpr/domain/NarudzbaProizvod.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.domain;
 
 public class NarudzbaProizvod implements Idable{
+    private int id;
+    private Proizvod proizvod;
+    private Narudzba narudzba;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Proizvod getProizvod() {
+        return proizvod;
+    }
+
+    public void setProizvod(Proizvod proizvod) {
+        this.proizvod = proizvod;
+    }
+
+    public Narudzba getNarudzba() {
+        return narudzba;
+    }
+
+    public void setNarudzba(Narudzba narudzba) {
+        this.narudzba = narudzba;
+    }
 }
