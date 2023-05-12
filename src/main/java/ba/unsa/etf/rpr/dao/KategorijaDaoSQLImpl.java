b820cc4d7156f59505bffc287c89f0393b643993
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-12 21:49:04 +0200

    Pocetak implementacije KategorijaDAOSQL

--- src/main/java/ba/unsa/etf/rpr/dao/KategorijaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KategorijaDaoSQLImpl.java
@@ -1,4 +1,47 @@
 package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.Kategorija;
 
-public class KategorijaDaoSQLImpl {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+import ba.unsa.etf.rpr.dao.KategorijaDao;
+import ba.unsa.etf.rpr.dao.KategorijaDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Kategorija;
+
+public class KategorijaDaoSQLImpl implements KategorijaDao {
+
+    private Connection connection;
+
+    public KategorijaDaoSQLImpl() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/freedb_Baza/RPR/2022/2023", "freedb_nhelac1", "5C9@sNnN&W5UyAE");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public Kategorija getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Kategorija add(Kategorija item) {
+        return null;
+    }
+
+    @Override
+    public Kategorija update(Kategorija item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+    }
+
+    @Override
+    public List<Kategorija> getAll() {
+       return null;
+    }
+
 }
