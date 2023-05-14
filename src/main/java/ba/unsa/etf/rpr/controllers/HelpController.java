6b8a2988e38147dfe5d1a41ae0a6540f53c77463
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-14 16:55:44 +0200

    Zapoceta o nama stranica

--- src/main/java/ba/unsa/etf/rpr/controllers/HelpController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HelpController.java
@@ -1,10 +1,36 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.HelloApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class HelpController {
+
+    public void actionOtvaranjeONama(ActionEvent actionEvent) throws IOException {
+        try {
+            Stage stage1 = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/obrendu.fxml"));
+            OBrenduController brend = new OBrenduController();
+            fxmlLoader.setController(brend);
+            Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage1.setTitle("CeraVe");
+            stage1.setScene(scene);
+            stage1.setResizable(false);
+            stage1.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     @FXML
     public void promjenaBoje1(MouseEvent event) {
         Button btn = (Button) event.getSource();
--- src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
@@ -47,6 +47,22 @@ public class PocetnaController {
             System.out.println(e.getMessage());
         }
     }
+
+    public void actionOtvaranjeONama(ActionEvent actionEvent) throws IOException {
+        try {
+            Stage stage1 = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/obrendu.fxml"));
+            OBrenduController brend = new OBrenduController();
+            fxmlLoader.setController(brend);
+            Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage1.setTitle("CeraVe");
+            stage1.setScene(scene);
+            stage1.setResizable(false);
+            stage1.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     @FXML
     public void promjenaBoje(MouseEvent event) {
         Button btn = (Button) event.getSource();
