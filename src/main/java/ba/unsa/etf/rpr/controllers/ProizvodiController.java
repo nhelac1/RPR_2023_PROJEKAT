4407a8a4504c7ab5929c90cf34e18620b190459a
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-14 21:03:59 +0200

    Uradjena odjava

--- src/main/java/ba/unsa/etf/rpr/controllers/ProizvodiController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProizvodiController.java
@@ -1,4 +1,87 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.*;
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class ProizvodiController {
+    public Button btnOdjava;
+    public ImageView imageOdjava;
+    public void actionOtvaranjeHelp(ActionEvent actionEvent) throws IOException {
+        try {
+            Stage stage1 = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/help.fxml"));
+            HelpController help = new HelpController();
+            fxmlLoader.setController(help);
+            Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage1.setTitle("Pomoć CeraVe");
+            stage1.setScene(scene);
+            stage1.setResizable(false);
+            stage1.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void actionOtvaranjeONama(ActionEvent actionEvent) throws IOException {
+        try {
+            /*Stage stage =(Stage)btnONama.getScene().getWindow();
+            stage.close();*/
+            Stage stage1 = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/obrendu.fxml"));
+            OBrenduController brend = new OBrenduController();
+            fxmlLoader.setController(brend);
+            Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage1.setTitle("CeraVe");
+            stage1.setScene(scene);
+            stage1.setResizable(false);
+            stage1.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void actionOdjaviSe(ActionEvent actionEvent) throws IOException {
+        try {
+            Stage stage =(Stage)btnOdjava.getScene().getWindow();
+            stage.close();
+            Stage stage1 = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/pocetna.fxml"));
+            PocetnaController pocetna = new PocetnaController();
+            fxmlLoader.setController(pocetna);
+            Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage1.setTitle("Online CeraVe");
+            stage1.setScene(scene);
+            stage1.setResizable(false);
+            stage1.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    @FXML
+    public void promjenaBoje1(MouseEvent event) {
+        Button btn = (Button) event.getSource();
+        btn.setStyle("-fx-background-color: rgb(128,188,255);");
+    }
+    @FXML
+    public void vracanjeBoje1(MouseEvent event) {
+        Button btn = (Button) event.getSource();
+        btn.setStyle("-fx-background-color: rgb(36,140,255);");
+    }
+
 }
