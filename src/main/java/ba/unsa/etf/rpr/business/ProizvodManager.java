3c4feaffe7bb22f6f87c9fd7bb500126c38b3148
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 20:59:27 +0200

    Metoda dobijanja svih proizvoda

--- src/main/java/ba/unsa/etf/rpr/business/ProizvodManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ProizvodManager.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Proizvod;
+import ba.unsa.etf.rpr.exceptions.CeraVeException;
+
+import java.util.List;
+
 public class ProizvodManager {
+    public List<Proizvod> dajSveProizvode() throws CeraVeException {
+        return DaoFactory.proizvodDao().getAll();
+    }
 }
