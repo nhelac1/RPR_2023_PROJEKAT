9b0adb7761172cde9a4b2de975fe9e479bcec2a6
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 20:52:04 +0200

    Metoda dodavanja za međutabelu

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaProizvodManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaProizvodManager.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.NarudzbaProizvod;
+import ba.unsa.etf.rpr.exceptions.CeraVeException;
+
 public class NarudzbaProizvodManager {
+    public NarudzbaProizvod dodajNP(NarudzbaProizvod np) throws CeraVeException {
+        return DaoFactory.narProDao().add(np);
+    }
 }
