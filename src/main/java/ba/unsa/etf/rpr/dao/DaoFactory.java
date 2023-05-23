108836f15d5f92b16d4f9b5a90ae136341820e35
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 19:28:02 +0200

    Azuriran DaoFactory

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -6,27 +6,19 @@ public class DaoFactory {
 
     private DaoFactory(){}
 
-
-
     private static final KategorijaDao kategorijaDao = KategorijaDaoSQLImpl.getInstance();
-
-    public static KategorijaDao kategorijaDao() {
-        return kategorijaDao;
-    }
-
-
+    public static KategorijaDao kategorijaDao() { return kategorijaDao; }
 
     private static final KorisnikDao korisnikDao = KorisnikDaoSQLImpl.getInstance();
-
     public static KorisnikDao korisnikDao() {
         return korisnikDao;
     }
 
-
-    
     private static final NarudzbaDao narudzbaDao = NarudzbaDaoSQLImpl.getInstance();
-
     public static NarudzbaDao narudzbaDao() {
         return narudzbaDao;
     }
+
+    private static final ProizvodDao proizvodDao = ProizvodDaoSQLImpl.getInstance();
+    public static ProizvodDao proizvodDao() { return proizvodDao; }
 }
