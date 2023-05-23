f8b6fffa86239fa673b8f56e7901e7221773148c
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 20:49:50 +0200

    Metoda dobijanja svih narudzbi

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
@@ -4,9 +4,15 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Narudzba;
 import ba.unsa.etf.rpr.exceptions.CeraVeException;
 
+import java.util.List;
+
 public class NarudzbaManager {
-    
+
     public Narudzba dodajNarudzbu(Narudzba n) throws CeraVeException {
         return DaoFactory.narudzbaDao().add(n);
+    }
+
+    public List<Narudzba> dajSveNarudzbe() throws CeraVeException {
+        return DaoFactory.narudzbaDao().getAll();
     }
 }
