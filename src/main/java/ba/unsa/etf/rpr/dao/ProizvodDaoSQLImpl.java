7c785f2a2d527f0fca6f1daf1bdfec780c7133b4
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 18:11:18 +0200

    Izbrisan sadržaj ProizvodDaoSQLImpl.java

--- src/main/java/ba/unsa/etf/rpr/dao/ProizvodDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ProizvodDaoSQLImpl.java
@@ -1,44 +1,5 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.domain.NarudzbaProizvod;
-import ba.unsa.etf.rpr.domain.Proizvod;
+public class ProizvodDaoSQLImpl  {
 
-import java.sql.*;
-import java.util.List;
-
-public class ProizvodDaoSQLImpl implements ProizvodDao {
-    private Connection connection;
-
-    public ProizvodDaoSQLImpl() {
-        try {
-            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/freedb_Baza/RPR/2022/2023", "freedb_nhelac1", "5C9@sNnN&W5UyAE");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    @Override
-    public Proizvod getById(int id){
-        return null;
-    }
-
-    @Override
-    public Proizvod add(Proizvod item){
-        return null;
-    }
-
-    @Override
-    public Proizvod update(Proizvod item){
-        return null;
-    }
-
-    @Override
-    public void delete(int id){
-
-    }
-
-    @Override
-    public List<Proizvod> getAll(){
-        return null;
-    }
 }
