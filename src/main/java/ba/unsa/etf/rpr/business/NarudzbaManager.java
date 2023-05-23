46c7c88b0c6390ba10af5686229f7ab2cc924178
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 20:48:46 +0200

    Metoda dodavanja nove narudzbe

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaManager.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Narudzba;
+import ba.unsa.etf.rpr.exceptions.CeraVeException;
+
 public class NarudzbaManager {
+    
+    public Narudzba dodajNarudzbu(Narudzba n) throws CeraVeException {
+        return DaoFactory.narudzbaDao().add(n);
+    }
 }
