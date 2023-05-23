6ea5d3f010e4f8ca45cd62d4715675fa6b8adfd5
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 18:35:13 +0200

    Implementacija KategorijaDaoSQLImpl.java

--- src/main/java/ba/unsa/etf/rpr/dao/KategorijaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KategorijaDaoSQLImpl.java
@@ -1,6 +1,45 @@
 package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.Kategorija;
+import ba.unsa.etf.rpr.exceptions.CeraVeException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class KategorijaDaoSQLImpl {
+public class KategorijaDaoSQLImpl extends AbstractDao<Kategorija> implements KategorijaDao {
+    private static KategorijaDaoSQLImpl instance = null;
+    private KategorijaDaoSQLImpl() {
+        super("Kategorija");
+    }
+
+    public static KategorijaDaoSQLImpl getInstance() {
+        if (instance == null)
+            instance = new KategorijaDaoSQLImpl();
+        return instance;
+    }
+    public static void removeInstance() {
+        if (instance != null)
+            instance = null;
+    }
 
 
+    @Override
+    public Kategorija row2object(ResultSet rs) throws CeraVeException {
+        try {
+            Kategorija kategorija = new Kategorija();
+            kategorija.setId(rs.getInt("id"));
+            kategorija.setIme(rs.getString("ime"));
+            return kategorija;
+        } catch (SQLException e) {
+            throw new CeraVeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Kategorija object) {
+        Map<String, Object> red = new TreeMap<>();
+        red.put("id", object.getId());
+        red.put("ime", object.getIme());
+        return red;
+    }
 }
