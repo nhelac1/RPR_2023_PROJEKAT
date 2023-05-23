386196bdacbb26e7c481444f5e441e8bbf3db242
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 20:06:55 +0200

    Metoda validacije imena kategorije

--- src/main/java/ba/unsa/etf/rpr/business/KategorijaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KategorijaManager.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.Kategorija;
+import ba.unsa.etf.rpr.exceptions.CeraVeException;
+
 public class KategorijaManager {
+    public void validacijaImenaKategorije(String ime) throws CeraVeException {
+        if (ime == null || ime.length() < 5)
+            throw new CeraVeException("Dužina imena kategorije mora biti veća od 5 !");
+
+    }
+
+    
 }
