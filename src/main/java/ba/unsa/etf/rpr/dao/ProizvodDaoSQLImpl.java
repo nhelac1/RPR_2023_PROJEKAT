38b9cfd51a2402b40781cee621ec4fd631cacc5b
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 19:26:41 +0200

    Nedovrsena implementacija NarudzbaDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/ProizvodDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ProizvodDaoSQLImpl.java
@@ -1,5 +1,53 @@
 package ba.unsa.etf.rpr.dao;
 
-public class ProizvodDaoSQLImpl  {
+import ba.unsa.etf.rpr.domain.Proizvod;
+import ba.unsa.etf.rpr.exceptions.CeraVeException;
 
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ProizvodDaoSQLImpl extends AbstractDao<Proizvod> implements ProizvodDao {
+
+    private static ProizvodDaoSQLImpl instance = null;
+
+    public ProizvodDaoSQLImpl() {
+        super("Proizvod");
+    }
+    public static ProizvodDaoSQLImpl getInstance() {
+        if(instance == null)
+            instance = new ProizvodDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance() {
+        if(instance != null)
+            instance = null;
+    }
+
+    @Override
+    public Proizvod row2object(ResultSet rs) throws CeraVeException {
+        try {
+            Proizvod proizvod = new Proizvod();
+            proizvod.setId(rs.getInt("id"));
+            proizvod.setIme(rs.getString("ime"));
+            proizvod.setCijena(rs.getString("cijena"));
+            //proizvod.setKategorija();
+            proizvod.setNamjena(rs.getString("namjena"));
+            return proizvod;
+        } catch (Exception e) {
+            throw new CeraVeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Proizvod object) {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("ime", object.getIme());
+        item.put("cijena", object.getCijena());
+        item.put("id_kategorija", object.getKategorija().getId());
+        item.put("namjena", object.getNamjena());
+        return item;
+    }
 }
