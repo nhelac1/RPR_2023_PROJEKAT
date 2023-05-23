e18d987f69a2d8eaa9dd9ced54e0f38189bf2e43
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 20:42:07 +0200

    Metoda za brisanje korisnika

--- src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
@@ -6,7 +6,19 @@ import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.CeraVeException;
 
 public class KorisnikManager {
+
     public static Korisnik pronadjiEmail(String email) throws CeraVeException {
         return DaoFactory.korisnikDao().pronadjiEmail(email);
+    }
+    public void obrisiKorisnika(int id_korisnik) throws CeraVeException {
+        try {
+            DaoFactory.korisnikDao().delete(id_korisnik);
+        } catch (CeraVeException e) {
+            if (e.getMessage().contains("FOREIGN KEY")) {
+                throw new CeraVeException("Ne možete obrisati korisnika, jer je u vezi sa tabelom Narudžba !");
+            }
+            throw e;
+        }
+
     }
 }
