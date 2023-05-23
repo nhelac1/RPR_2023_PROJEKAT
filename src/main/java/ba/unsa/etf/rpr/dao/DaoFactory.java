59bd49f75d8b1540c6d01bc81d9efeac5f0566e7
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 19:07:03 +0200

    Nedovrsena implementacija za NarudzbaDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,6 +1,22 @@
 package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
+
+    private DaoFactory(){}
+
+
+
     private static final KategorijaDao kategorijaDao = KategorijaDaoSQLImpl.getInstance();
+
+    public static KategorijaDao kategorijaDao() {
+        return kategorijaDao;
+    }
+
+
+
     private static final KorisnikDao korisnikDao = KorisnikDaoSQLImpl.getInstance();
+
+    public static KorisnikDao korisnikDao() {
+        return korisnikDao;
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDaoSQLImpl.java
@@ -1,5 +1,49 @@
 package ba.unsa.etf.rpr.dao;
 
-public class NarudzbaDaoSQLImpl{
+import ba.unsa.etf.rpr.domain.Narudzba;
+import ba.unsa.etf.rpr.exceptions.CeraVeException;
 
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class NarudzbaDaoSQLImpl extends AbstractDao<Narudzba> implements NarudzbaDao {
+    private static NarudzbaDaoSQLImpl instance = null;
+    public NarudzbaDaoSQLImpl() {
+        super("Narudzba");
+    }
+
+    public static NarudzbaDaoSQLImpl getInstance()
+    {
+        if(instance == null)
+            instance = new NarudzbaDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance()
+    {
+        if(instance != null)
+            instance = null;
+    }
+    @Override
+    public Narudzba row2object(ResultSet rs) throws CeraVeException {
+        try {
+            Narudzba narudzba = new Narudzba();
+            narudzba.setId(rs.getInt("id"));
+            narudzba.setCijena(rs.getFloat("cijena"));
+            //narudzba.setKorisnik();
+            return narudzba;
+        } catch (Exception e) {
+            throw new CeraVeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Narudzba object) {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("cijena", object.getCijena());
+        item.put("id_korisnik", object.getKorisnik().getId());
+        return item;
+    }
 }
