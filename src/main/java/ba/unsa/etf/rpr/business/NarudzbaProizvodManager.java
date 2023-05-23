e882b6276c824e198033dc0ad116b2c57f15ecef
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-05-23 20:53:30 +0200

    Metoda dobijanja svih međutabela

--- src/main/java/ba/unsa/etf/rpr/business/NarudzbaProizvodManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/NarudzbaProizvodManager.java
@@ -4,8 +4,13 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.NarudzbaProizvod;
 import ba.unsa.etf.rpr.exceptions.CeraVeException;
 
+import java.util.List;
+
 public class NarudzbaProizvodManager {
     public NarudzbaProizvod dodajNP(NarudzbaProizvod np) throws CeraVeException {
         return DaoFactory.narProDao().add(np);
+    }
+    public List<NarudzbaProizvod> dajSveNP() throws CeraVeException {
+        return DaoFactory.narProDao().getAll();
     }
 }
