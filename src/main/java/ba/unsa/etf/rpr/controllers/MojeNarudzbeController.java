df09d7da3b95e428beb2da4943801c85e72da064
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-06-05 23:07:20 +0200

    Izmjene u meniju

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/MojeNarudzbeController.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class MojeNarudzbeController {
+
+
+    @FXML
+    public void promjenaBoje(MouseEvent event) {
+        Button btn = (Button) event.getSource();
+        btn.setStyle("-fx-background-color: rgb(196,196,196); -fx-background-radius:10px;");
+    }
+
+    @FXML
+    public void vracanjeBoje(MouseEvent event) {
+        Button btn = (Button) event.getSource();
+        btn.setStyle("-fx-background-color: rgb(223,223,223); -fx-background-radius: 10px;");
+    }
+
+
+
+}
