5ef98b15a3cfc8a55e0f4acaf1c1405082526833
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-06-05 18:58:17 +0200

    Test1 - za provjeru konstruktora klase Korisnik

--- src/test/java/ba/unsa/etf/rpr/ApplicationTest.java
+++ src/test/java/ba/unsa/etf/rpr/ApplicationTest.java
@@ -1,11 +1,22 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.domain.Korisnik;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
+    Korisnik k1 = new Korisnik(1, "Nedžla", "Helać", "Zmaja od Bosne bb", "nedzlah@gmail.com", "12345678");
+
     @Test
     public void Test1() {
-
+        assertAll(
+                () -> assertEquals(1, k1.getId(), "ID je netačan!"),
+                () -> assertEquals("Nedžla", k1.getIme(), "Ime je netačno!"),
+                () -> assertEquals("Helać", k1.getPrezime(), "Prezime je netačno!"),
+                () -> assertEquals("Zmaja od Bosne bb", k1.getAdresa(), "Adresa je netačna!"),
+                () -> assertEquals("nedzlah@gmail.com", k1.getEmail(), "Email je netačan!"),
+                () -> assertEquals("12345678", k1.getPassword(), "Password je netačan!")
+        );
     }
     @Test
     public void Test2() {
