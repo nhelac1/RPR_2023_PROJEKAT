094b0e404237e9582c92d1288866357b09971ecb
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-06-05 21:18:01 +0200

    Java Doc dokumentacija za dao klasu narudzba

--- src/main/java/ba/unsa/etf/rpr/dao/ProizvodDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ProizvodDao.java
@@ -5,7 +5,11 @@ import ba.unsa.etf.rpr.domain.Proizvod;
 import ba.unsa.etf.rpr.exceptions.CeraVeException;
 
 import java.util.List;
-
+/**
+ * DAO interfejs za objekat Proizvod
+ *
+ * @author Nedzla
+ */
 public interface ProizvodDao extends Dao<Proizvod> {
     List<Proizvod> pronadjiProizvod(String text) throws CeraVeException;
     List<Proizvod> pronadjiProizvodPoKategoriji(Kategorija kat) throws CeraVeException;
