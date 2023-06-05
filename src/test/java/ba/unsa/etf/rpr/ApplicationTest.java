2fb084a05c80192d51be0c8277160f3bace1bd90
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-06-05 18:39:10 +0200

    Zapocetno testiranje aplikacije

---
+++ src/test/java/ba/unsa/etf/rpr/ApplicationTest.java
@@ -0,0 +1,46 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    @Test
+    public void Test1() {
+
+    }
+    @Test
+    public void Test2() {
+
+    }
+    @Test
+    public void Test3() {
+
+    }
+    @Test
+    public void Test4() {
+
+    }
+    @Test
+    public void Test5() {
+
+    }
+    @Test
+    public void Test6() {
+
+    }
+    @Test
+    public void Test7() {
+
+    }
+    @Test
+    public void Test8() {
+
+    }
+    @Test
+    public void Test9() {
+
+    }
+    @Test
+    public void Test10() {
+
+    }
+}
