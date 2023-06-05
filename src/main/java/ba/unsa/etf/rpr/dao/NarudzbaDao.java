ff59162fb3375e6e6cb90d92e4b58ffc4f6f5738
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-06-05 21:17:47 +0200

    Java Doc dokumentacija za dao klasu narudzba

--- src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDao.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Narudzba;
-
+/**
+ * DAO interfejs za objekat Narudzba
+ *
+ * @author Nedzla
+ */
 public interface NarudzbaDao extends Dao<Narudzba> {
 }
