4194d23e0abc367e70856a943dcc44b777068388
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-06-05 21:17:31 +0200

    Java Doc dokumentacija za dao klasu korisnik

--- src/main/java/ba/unsa/etf/rpr/dao/KorisnikDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KorisnikDao.java
@@ -2,7 +2,11 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.CeraVeException;
-
+/**
+ * DAO interfejs za objekat Korisnik
+ *
+ * @author Nedzla
+ */
 public interface KorisnikDao extends Dao<Korisnik> {
     Korisnik pronadjiEmail(String email) throws CeraVeException;
 }
