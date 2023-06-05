a885c5c8c673c583b0576c1db674bcfa543d8dd3
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-06-05 21:18:21 +0200

    Java Doc dokumentacija za dao klasu narudzbaproizvod

--- src/main/java/ba/unsa/etf/rpr/dao/NarudzbaProizvodDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/NarudzbaProizvodDao.java
@@ -3,7 +3,11 @@ import ba.unsa.etf.rpr.domain.NarudzbaProizvod;
 import ba.unsa.etf.rpr.exceptions.CeraVeException;
 
 import java.util.List;
-
+/**
+ * DAO interfejs za objekat NarudzbaProizvod
+ *
+ * @author Nedzla
+ */
 public interface NarudzbaProizvodDao extends Dao<NarudzbaProizvod> {
     List<NarudzbaProizvod> dajPoKorisniku(int id_korisnik) throws CeraVeException;
 }
