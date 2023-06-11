0b795c3e17fb67288139e1315bb10400d10c3429
Nedzla Helac <nhelac1@etf.unsa.ba>
2023-06-11 13:20:35 +0200

    Klasa za pokretanje kroz CMD

---
+++ src/main/java/ba/unsa/etf/rpr/HelloApplication2.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HelloApplication2 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
